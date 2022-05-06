9632f8166088ab4d04a82e43622be3894659e6ac
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-05-06 00:10:18 -0400

    fix error

--- 85_stack/StackImpl.py
+++ 85_stack/StackImpl.py
@@ -6,13 +6,13 @@ class StackImpl(Stack):
     list = []
 
     def isEmpty(self):
-        return self.list.count() <= 0
+        return len(self.list) <= 0
 
     def peekTop(self):
-        return list[-1]
+        return self.list[-1]
 
     def pop(self):
-        return list.pop(-1)
+        return self.list.pop(-1)
 
     def push(self, Any):
-        list.append[Any]
+        self.list.append(Any)
